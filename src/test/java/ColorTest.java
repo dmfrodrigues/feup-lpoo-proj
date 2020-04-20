82963d8ad5590535372d9be75e4d1b797bb17975
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-04-20 19:55:02 +0100

    Minor fix to Color test

--- src/test/java/ColorTest.java
+++ src/test/java/ColorTest.java
@@ -8,9 +8,9 @@ public class ColorTest {
     @Test
     public void getters(){
         for(int i = 0; i < GETTERS_NUM_TESTS; ++i){
-            int r = (int)Math.random()*MAX_COLOR,
-                    g = (int)Math.random()*MAX_COLOR,
-                    b = (int)Math.random()*MAX_COLOR;
+            int r = (int)Math.random()*MAX_COLOR;
+            int g = (int)Math.random()*MAX_COLOR;
+            int b = (int)Math.random()*MAX_COLOR;
             Color c = new Color(r, g, b);
             assertEquals(r, c.getR());
             assertEquals(g, c.getG());
