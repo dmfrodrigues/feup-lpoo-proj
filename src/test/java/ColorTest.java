90b3b54786825bca52afb854ede65bf347c51a4d
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-04-20 19:06:59 +0100

    Added Color

---
+++ src/test/java/ColorTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ColorTest {
+    final int MAX_COLOR = 255;
+    final int GETTERS_NUM_TESTS = 100;
+    @Test
+    public void getters(){
+        for(int i = 0; i < GETTERS_NUM_TESTS; ++i){
+            int r = (int)Math.random()*MAX_COLOR,
+                    g = (int)Math.random()*MAX_COLOR,
+                    b = (int)Math.random()*MAX_COLOR;
+            Color c = new Color(r, g, b);
+            assertEquals(r, c.getR());
+            assertEquals(g, c.getG());
+            assertEquals(b, c.getB());
+        }
+    }
+}
