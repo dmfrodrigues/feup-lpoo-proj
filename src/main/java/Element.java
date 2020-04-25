d7d09c029ebedceed8de2bf7a8241d2f5ad106b0
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-04-25 18:37:07 +0100

    Added equals override to Element (also checks if classes are different)

--- src/main/java/Element.java
+++ src/main/java/Element.java
@@ -7,4 +7,15 @@ public abstract class Element {
     public Position getPos(){
         return new Position(pos);
     }
+
+    /**
+     * Override of equals. Also checks if they are the same class.
+     * @param obj   Object to compare with
+     * @return      True if they are equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object obj) {
+        return (getClass().equals(obj.getClass()) &&
+                pos.equals(((Element)obj).pos));
+    }
 }
