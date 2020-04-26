a67c104fdfc8ba0311347e2626680309ac0acc3d
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-04-26 23:12:23 +0100

    Hotfix

--- src/main/java/DynamicElement.java
+++ src/main/java/DynamicElement.java
@@ -1,36 +1,36 @@
 public abstract class DynamicElement extends Element {
-    protected Direction dir;
+    protected Application.Direction dir;
     public DynamicElement(Position pos){
         super(pos);
-        dir = Direction.RIGHT;
+        dir = Application.Direction.RIGHT;
     }
 
     public void moveUp(){
         Position pos = getPos();
         pos.setY(pos.getY()-1);
         setPos(pos);
-        dir = Direction.UP;
+        dir = Application.Direction.UP;
     }
     public void moveDown(){
         Position pos = getPos();
         pos.setY(pos.getY()+1);
         setPos(pos);
-        dir = Direction.DOWN;
+        dir = Application.Direction.DOWN;
     }
     public void moveLeft(){
         Position pos = getPos();
         pos.setX(pos.getX()-1);
         setPos(pos);
-        dir = Direction.LEFT;
+        dir = Application.Direction.LEFT;
     }
     public void moveRight(){
         Position pos = getPos();
         pos.setX(pos.getX()+1);
         setPos(pos);
-        dir = Direction.RIGHT;
+        dir = Application.Direction.RIGHT;
 
     }
 
-    public Direction getDirection() { return dir; }
+    public Application.Direction getDirection() { return dir; }
 
 }
--- src/main/java/TerminalSpriteOrientable.java
+++ src/main/java/TerminalSpriteOrientable.java
@@ -2,19 +2,22 @@ import java.io.InputStream;
 
 public class TerminalSpriteOrientable{
     private TerminalSprite spriteRight, spriteLeft, spriteUp, spriteDown;
-    public TerminalSpriteOrientable(TerminalSprite sprite){
+    public TerminalSpriteOrientable(TerminalSprite sprite) throws IllegalArgumentException{
+        if(sprite == null) throw new IllegalArgumentException("Null sprite not allowed");
         spriteRight = sprite;
     }
     public void setSpriteRight(TerminalSprite sprite){ this.spriteRight = sprite; }
     public void setSpriteLeft(TerminalSprite sprite){ this.spriteLeft = sprite; }
     public void setSpriteUp(TerminalSprite sprite){ this.spriteUp = sprite; }
     public void setSpriteDown(TerminalSprite sprite){ this.spriteDown = sprite; }
-    public TerminalSprite getSprite(Direction dir){
-        String s = dir.getClass().toString();
-        if(s.equals("class NorthDirection") && spriteUp    != null) return spriteUp;
-        if(s.equals("class SouthDirection") && spriteDown  != null) return spriteDown;
-        if(s.equals("class WestDirection" ) && spriteLeft  != null) return spriteLeft;
-        if(s.equals("class EastDirection" ) && spriteRight != null) return spriteRight;
-        return spriteRight;
+    public TerminalSprite getSprite(Application.Direction dir){
+        TerminalSprite res = null;
+        switch(dir) {
+            case UP   : res = spriteUp   ; break;
+            case DOWN : res = spriteDown ; break;
+            case LEFT : res = spriteLeft ; break;
+            case RIGHT: res = spriteRight; break;
+        }
+        return (res != null ? res : spriteRight);
     }
 }
