21cae6dc1603849ca68255f265bf1be449a8b413
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-04-26 15:36:08 +0100

    Added support for color from Hex string

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -5,6 +5,13 @@ public class Color {
         this.g = g;
         this.b = b;
     }
+    public Color(String hex) throws IllegalArgumentException {
+        if(hex.length() != 7) throw new IllegalArgumentException("Argument has wrong length");
+        if(hex.charAt(0) != '#') throw new IllegalArgumentException("Argument has missing initial '#'");
+        this.r = Integer.parseInt(hex.substring(1, 3));
+        this.g = Integer.parseInt(hex.substring(3, 5));
+        this.b = Integer.parseInt(hex.substring(5, 7));
+    }
     public int getR(){ return r; }
     public int getG(){ return g; }
     public int getB(){ return b; }
