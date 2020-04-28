a6f2e06c9a97125a4cb9468dc2a808e740225fed
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-04-29 00:53:15 +0100

    Refactored

--- src/main/java/com/pacman/g60/Model/ArenaModelLoaderStream.java
+++ src/main/java/com/pacman/g60/Model/ArenaModelLoaderStream.java
@@ -1,27 +1,40 @@
 package com.pacman.g60.Model;
 
+import com.pacman.g60.Model.Elements.Element;
+
 import java.io.InputStream;
 import java.util.Scanner;
 
 public class ArenaModelLoaderStream implements ArenaModel.Loader {
+    private class Factory {
+        private Position pos;
+        public Factory(Position pos){ this.pos = new Position(pos); }
+        public Element factoryMethod(char c) throws IllegalArgumentException {
+            switch(c){
+                case 'W': return new Wall(pos);
+                case 'H': return new Hero(pos);
+                case 'G': return new Ghost(pos);
+                case ' ': return null;
+                default: throw new IllegalArgumentException("Unknown character");
+            }
+        }
+    }
+    
     private ArenaModel arenaModel;
+    
     public ArenaModelLoaderStream(InputStream inputStream) {
         Scanner scanner = new Scanner(inputStream);
-        String firstLine = scanner.nextLine();
-        Integer W = Integer.parseInt(firstLine.split(" ")[0]);
-        Integer H = Integer.parseInt(firstLine.split(" ")[1]);
+        String[] firstLine = scanner.nextLine().split(" ");
+        Integer W = Integer.parseInt(firstLine[0]);
+        Integer H = Integer.parseInt(firstLine[1]);
         arenaModel = new ArenaModel(W, H);
         Position pos;
         for(Integer y = 0; y < H; ++y){
             String line = scanner.nextLine();
             for(Integer x = 0; x < W; ++x){
-                switch(line.charAt(x)){
-                    case 'W': arenaModel.addElement(new Wall(new Position(x, y))); break;
-                    case 'H': arenaModel.addElement(new Hero(new Position(x, y))); break;
-                    case 'G': arenaModel.addElement(new Ghost(new Position(x, y))); break;
-                    case ' ': break;
-                    default: throw new IllegalArgumentException("Unknown character");
-                }
+                ArenaModelLoaderStream.Factory factory = new ArenaModelLoaderStream.Factory(new Position(x, y));
+                Element element = factory.factoryMethod(line.charAt(x));
+                if(element != null) arenaModel.addElement(element);
             }
         }
     }
