e15c19b11921fbd639497873ec5bc8b8c0aab9a0
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-04-29 02:14:38 +0100

    Done refactoring

--- src/main/java/com/pacman/g60/View/TerminalArenaView.java
+++ src/main/java/com/pacman/g60/View/TerminalArenaView.java
@@ -3,79 +3,29 @@ package com.pacman.g60.View;
 
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
-import com.pacman.g60.Model.ArenaModel;
+import com.pacman.g60.Model.*;
 import com.pacman.g60.Model.Elements.DynamicElement;
 import com.pacman.g60.Model.Elements.Element;
-import com.pacman.g60.Model.Elements.StaticElement;
-import com.pacman.g60.Model.Position;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class TerminalArenaView implements ArenaView {
-    private TerminalGUI terminalGUI;
-
-    private Map<String, TerminalSprite> spriteMap;
-    private Map<String, TerminalSpriteOrientable> spriteOrientableMap;
-
-    public TerminalArenaView(TerminalGUI terminalGUI){
-        this.terminalGUI = terminalGUI;
-        spriteMap = new HashMap<>();
-        spriteOrientableMap = new HashMap<>();
-
-        try {
-            TerminalSprite.Loader loader;
-            TerminalSpriteOrientable spriteOrientable;
-            //Wall
-            loader = new TerminalSpriteLoaderFile(new FileInputStream("src/main/resources/lanterna-sprites/wall-8-4.lan"));
-            spriteMap.put("class com.pacman.g60.Model.Wall", loader.getTerminalSprite());
-            //Hero
-            loader = new TerminalSpriteLoaderFile(new FileInputStream("src/main/resources/lanterna-sprites/hero-8-4-right.lan"));
-            spriteOrientable = new TerminalSpriteOrientable(loader.getTerminalSprite());
-            loader = new TerminalSpriteLoaderFile(new FileInputStream("src/main/resources/lanterna-sprites/hero-8-4-left.lan"));
-            spriteOrientable.setSpriteLeft(loader.getTerminalSprite());
-            loader = new TerminalSpriteLoaderFile(new FileInputStream("src/main/resources/lanterna-sprites/hero-8-4-up.lan"));
-            spriteOrientable.setSpriteUp(loader.getTerminalSprite());
-            loader = new TerminalSpriteLoaderFile(new FileInputStream("src/main/resources/lanterna-sprites/hero-8-4-down.lan"));
-            spriteOrientable.setSpriteDown(loader.getTerminalSprite());
-            spriteOrientableMap.put("class com.pacman.g60.Model.Hero", spriteOrientable);
-            //Ghost
-            loader = new TerminalSpriteLoaderFile(new FileInputStream("src/main/resources/lanterna-sprites/ghost-8-4-right.lan"));
-            spriteOrientable = new TerminalSpriteOrientable(loader.getTerminalSprite());
-            loader = new TerminalSpriteLoaderFile(new FileInputStream("src/main/resources/lanterna-sprites/ghost-8-4-left.lan"));
-            spriteOrientable.setSpriteLeft(loader.getTerminalSprite());
-            spriteOrientableMap.put("class com.pacman.g60.Model.Ghost", spriteOrientable);
-        } catch(FileNotFoundException e){
-            System.err.println("Failed to find file");
-        }
-
-    }
-
-    public class ElementView {
+    
+    private abstract class ElementView {
         private final static int Wtile = 8;
         private final static int Htile = 4;
-        private final Position heroPos;
         
-        public ElementView(Position heroPos){
-            this.heroPos = heroPos;
-        }
-
-        public void draw(Element e){
-            Position pos = e.getPos();
-            TerminalSprite sprite;
-            
-            if(e instanceof StaticElement){
-                sprite = spriteMap.get(e.getClass().toString());
-            } else if(e instanceof DynamicElement) {
-                sprite = spriteOrientableMap.get(e.getClass().toString()).getSprite(((DynamicElement) e).getDirection());
-            } else return;
-            
-            int x0 = (pos.getX()-heroPos.getX())*Wtile + terminalGUI.getW()/2;
-            int y0 = (pos.getY()-heroPos.getY())*Htile + terminalGUI.getH()/2;
+        private Position heroPos;
+        public ElementView(){}
+        public final void setHeroPos(Position heroPos) { this.heroPos = heroPos; }
+        protected abstract TerminalSprite getSprite(Element e);
+        public final void draw(Element e){
+            TerminalSprite sprite = getSprite(e);
+            int x0 = (e.getPos().getX()-heroPos.getX())*Wtile + terminalGUI.getW()/2;
+            int y0 = (e.getPos().getY()-heroPos.getY())*Htile + terminalGUI.getH()/2;
             for(int x = 0; x < sprite.getW(); ++x) {
                 for (int y = 0; y < sprite.getH(); ++y) {
                     terminalGUI.drawCharacter(x0 + x, y0 + y,
@@ -85,16 +35,104 @@ public class TerminalArenaView implements ArenaView {
                 }
             }
         }
+    }
+    private class WallView extends ElementView {
+        TerminalSprite sprite;
+        
+        public WallView() throws FileNotFoundException{
+            TerminalSprite.Loader loader = new TerminalSpriteLoaderFile(new FileInputStream("src/main/resources/lanterna-sprites/wall-8-4.lan"));
+            sprite = loader.getTerminalSprite();
+        }
+        
+        @Override
+        protected TerminalSprite getSprite(Element e) {
+            return sprite;
+        }
+    }
+    private class HeroView extends ElementView {
+        TerminalSpriteOrientable spriteOrientable;
+        
+        public HeroView() throws FileNotFoundException {
+            TerminalSprite.Loader loader;
+            loader = new TerminalSpriteLoaderFile(new FileInputStream("src/main/resources/lanterna-sprites/hero-8-4-right.lan"));
+            spriteOrientable = new TerminalSpriteOrientable(loader.getTerminalSprite());
+            loader = new TerminalSpriteLoaderFile(new FileInputStream("src/main/resources/lanterna-sprites/hero-8-4-left.lan"));
+            spriteOrientable.setSpriteLeft(loader.getTerminalSprite());
+            loader = new TerminalSpriteLoaderFile(new FileInputStream("src/main/resources/lanterna-sprites/hero-8-4-up.lan"));
+            spriteOrientable.setSpriteUp(loader.getTerminalSprite());
+            loader = new TerminalSpriteLoaderFile(new FileInputStream("src/main/resources/lanterna-sprites/hero-8-4-down.lan"));
+            spriteOrientable.setSpriteDown(loader.getTerminalSprite());
+        }
 
+        @Override
+        protected TerminalSprite getSprite(Element e) {
+            return spriteOrientable.getSprite(((DynamicElement) e).getDirection());
+        }
+    }
+    private class GhostView extends ElementView {
+        TerminalSpriteOrientable spriteOrientable;
+
+        public GhostView() throws FileNotFoundException {
+            TerminalSprite.Loader loader;
+            loader = new TerminalSpriteLoaderFile(new FileInputStream("src/main/resources/lanterna-sprites/ghost-8-4-right.lan"));
+            spriteOrientable = new TerminalSpriteOrientable(loader.getTerminalSprite());
+            loader = new TerminalSpriteLoaderFile(new FileInputStream("src/main/resources/lanterna-sprites/ghost-8-4-left.lan"));
+            spriteOrientable.setSpriteLeft(loader.getTerminalSprite());
+        }
+
+        @Override
+        protected TerminalSprite getSprite(Element e) {
+            return spriteOrientable.getSprite(((DynamicElement) e).getDirection());
+        }
+    }
+    
+    private class ElementViewFactory {
+        private Position heroPos;
+        WallView wallView;
+        HeroView heroView;
+        GhostView ghostView;
+        public ElementViewFactory() throws FileNotFoundException{
+            wallView = new WallView();
+            heroView = new HeroView();
+            ghostView = new GhostView();
+        }
+
+        public void setHeroPos(Position heroPos) {
+            this.heroPos = heroPos;
+            wallView.setHeroPos(heroPos);
+            heroView.setHeroPos(heroPos);
+            ghostView.setHeroPos(heroPos);
+        }
+
+        public ElementView factoryMethod(Element e){
+            ElementView res = null;
+            try {
+                if     (e instanceof Wall ) res = wallView;
+                else if(e instanceof Hero ) res = heroView;
+                else if(e instanceof Ghost) res = ghostView;
+            } catch(Exception ex){
+                res = null;
+            }
+            return res;
+        }
+    }
+    
+    private TerminalGUI terminalGUI;
+    ElementViewFactory elementViewFactory;
+
+    public TerminalArenaView(TerminalGUI terminalGUI) throws FileNotFoundException{
+        this.terminalGUI = terminalGUI;
+        elementViewFactory = new ElementViewFactory();
     }
 
     @Override
     public void draw(ArenaModel arena) throws IOException {
         terminalGUI.clear();
         List<Element> listElements = arena.getElements();
-        ElementView elementViewTerminal = new ElementView(arena.getHero().getPos());
+        elementViewFactory.setHeroPos(arena.getHero().getPos());
         for(final Element e : listElements){
-            elementViewTerminal.draw(e);
+            ElementView elementView = elementViewFactory.factoryMethod(e);
+            elementView.draw(e);
         }
         terminalGUI.refresh();
     }
