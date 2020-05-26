ebf95cc5417afcef535f2a8ad62150a97d49484a
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-05-26 14:19:58 +0100

    Changed hierarchy

--- src/main/java/com/pacman/g60/Model/Elements/Collectable.java
+++ src/main/java/com/pacman/g60/Model/Elements/Collectable.java
@@ -3,8 +3,6 @@ package com.pacman.g60.Model.Elements;
 import com.pacman.g60.Model.Elements.Hierarchy.CanSharePosition;
 import com.pacman.g60.Model.Position;
 
-public class Coin extends Collectable {
-    public Coin(Position pos) {
-        super(pos);
-    }
+public abstract class Collectable extends StaticElement implements CanSharePosition {
+    public Collectable(Position pos){ super(pos); }
 }
