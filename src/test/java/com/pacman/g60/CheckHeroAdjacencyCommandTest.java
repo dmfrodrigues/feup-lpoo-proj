c481e181c5998c6393366a20055ea577427adff2
MechJM <matosjoaops@protonmail.com>
2020-05-27 21:51:26 +0100

    Added test for CheckHeroAdjacencyComand

--- src/test/java/com/pacman/g60/CheckHeroAdjacencyCommandTest.java
+++ src/test/java/com/pacman/g60/CheckHeroAdjacencyCommandTest.java
@@ -1,5 +1,6 @@
 package com.pacman.g60;
 
+import com.pacman.g60.Controller.CheckHeroAdjacencyCommand;
 import com.pacman.g60.Model.ArenaModel;
 import com.pacman.g60.Model.Effect.DamageEffect;
 import com.pacman.g60.Model.Elements.Element;
@@ -16,6 +17,8 @@ import org.mockito.stubbing.Answer;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class CheckHeroAdjacencyCommandTest {
     private ArenaModel arenaModel;
@@ -43,17 +46,34 @@ public class CheckHeroAdjacencyCommandTest {
         Mockito.when(arenaModel.getHero()).thenReturn(hero);
         Mockito.when(hero.getPos()).thenReturn(new Position(6,6));
         Mockito.doCallRealMethod().when(hero).setHealth(Mockito.anyInt());
+        Mockito.when(hero.getHealth()).thenCallRealMethod();
         hero.setHealth(10);
 
         DamageEffect effect1 = Mockito.mock(DamageEffect.class);
         DamageEffect effect2 = Mockito.mock(DamageEffect.class);
 
+        Mockito.when(ghost.getEffect()).thenReturn(effect1);
+        Mockito.when(ogre.getEffect()).thenReturn(effect2);
+
         Mockito.doAnswer(new Answer() {
             @Override
             public Object answer(InvocationOnMock invocation) throws Throwable {
-                hero.s
+                hero.setHealth(hero.getHealth() - 1);
                 return null;
             }
         }).when(effect1).apply(hero);
+
+        Mockito.doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                hero.setHealth(hero.getHealth() - 2);
+                return null;
+            }
+        }).when(effect2).apply(hero);
+
+        CheckHeroAdjacencyCommand command = new CheckHeroAdjacencyCommand(arenaModel);
+        command.execute();
+
+        assertEquals(Integer.valueOf(7),hero.getHealth());
     }
 }
