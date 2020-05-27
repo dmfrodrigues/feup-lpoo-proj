7bc2edccc39226a490d02812608e8499e12358fb
MechJM <matosjoaops@protonmail.com>
2020-05-27 20:22:46 +0100

    Added test for CheckForDeathCommand

---
+++ src/test/java/com/pacman/g60/CheckForDeathCommandTest.java
@@ -0,0 +1,60 @@
+package com.pacman.g60;
+
+import com.pacman.g60.Controller.CheckForDeathCommand;
+import com.pacman.g60.Model.ArenaModel;
+import com.pacman.g60.Model.Elements.Element;
+import com.pacman.g60.Model.Elements.Ghost;
+import com.pacman.g60.Model.Elements.Hero;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class CheckForDeathCommandTest {
+    private ArenaModel arenaModel;
+
+    @Before
+    public void setup()
+    {
+        arenaModel = Mockito.mock(ArenaModel.class);
+    }
+
+    @Test
+    public void test1()
+    {
+        Hero hero = Mockito.mock(Hero.class);
+        Mockito.when(hero.isAlive()).thenReturn(true);
+
+        Ghost ghost = Mockito.mock(Ghost.class);
+        Mockito.when(ghost.isAlive()).thenReturn(false);
+
+        ArrayList<Element> elems = new ArrayList<>();
+        elems.add(hero);
+        elems.add(ghost);
+
+        Mockito.when(arenaModel.getElements()).thenReturn(elems);
+        Mockito.doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Object obj = invocation.getArgument(0);
+                elems.remove(obj);
+                return null;
+            }
+        }).when(arenaModel).removeElement(Mockito.any(Element.class));
+
+        ArrayList<Element> expected = new ArrayList<>();
+        expected.add(hero);
+
+        CheckForDeathCommand command = new CheckForDeathCommand(arenaModel);
+        command.execute();
+
+        assertEquals(expected,arenaModel.getElements());
+    }
+}
