d76841e5c7386d9fe0138eba904e12d3843c5f0f
MechJM <matosjoaops@protonmail.com>
2020-05-30 19:42:36 +0100

    Added debug code

--- src/main/java/com/pacman/g60/Controller/ArenaController.java
+++ src/main/java/com/pacman/g60/Controller/ArenaController.java
@@ -2,10 +2,15 @@ package com.pacman.g60.Controller;
 
 
 import com.pacman.g60.Application;
+import com.pacman.g60.Model.Elements.Element;
+import com.pacman.g60.Model.Elements.Guard;
+import com.pacman.g60.Model.Elements.Hero;
 import com.pacman.g60.Model.Models.ArenaModel;
+import com.pacman.g60.Model.Position;
 import com.pacman.g60.View.Views.ArenaView;
 
 import java.io.IOException;
+import java.util.List;
 
 public class ArenaController extends Controller {
     private ArenaView arenaView;
@@ -37,6 +42,41 @@ public class ArenaController extends Controller {
 
             long numberUpdates = rateController.numberUpdatesInThisFrame();
             for(long i = 0; i < numberUpdates; ++i) {
+                //Debug
+                /*
+                List<Element> elems = arenaModel.getElements();
+                for (Element elem : elems)
+                {
+                    if (elem instanceof Guard && elem.getPos().getY() == 3)
+                    {
+                        System.out.println("Guard Position: ");
+                        Position pos = elem.getPos();
+                        System.out.println("X: " + pos.getX() + ", Y: " + pos.getY());
+
+                        List<Element> elemsInPos = arenaModel.getElemFromPos(pos);
+                        for (Element elem2 : elemsInPos)
+                        {
+                            if (elem2 instanceof Guard && pos.getY() == 3)
+                            {
+                                System.out.println("Guard in the same position");
+                                System.out.println("");
+                            }
+                        }
+                    }
+                }*/
+                List<Element> elems = arenaModel.getElemFromPos(arenaModel.getHero().getPos());
+                boolean foundHero = false;
+                boolean foundGuard = false;
+                for (Element elem : elems)
+                {
+                    if (elem instanceof Hero) foundHero = true;
+                    if (elem instanceof Guard) foundGuard = true;
+                    if (foundGuard && foundHero)
+                    {
+                        System.out.println("Guard and hero shared pos");
+                    }
+                }
+                //Debug
                 executeCommand(new UpdateAllEnemyPosCommand(this.arenaModel));
                 executeCommand(new CheckHeroAdjacencyCommand(this.arenaModel));
                 executeCommand(new CheckForDeathCommand(this.arenaModel));
