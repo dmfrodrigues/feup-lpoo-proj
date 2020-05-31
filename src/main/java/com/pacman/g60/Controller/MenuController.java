a1335dab7bf11674cc4565d0be86e954383129b4
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-05-31 17:31:50 +0100

    Improved FrameRateController

--- src/main/java/com/pacman/g60/Controller/MenuController.java
+++ src/main/java/com/pacman/g60/Controller/MenuController.java
@@ -1,43 +1,41 @@
 package com.pacman.g60.Controller;
 
 import com.pacman.g60.Model.Models.MenuModel;
+import com.pacman.g60.View.FrameRateController;
 import com.pacman.g60.View.Views.GUIView;
 
 import java.io.IOException;
 
 public class MenuController {
-    private MenuModel menuModel;
-    private GUIView view;
-    private UpdateRateController rateController = new UpdateRateController(30);
+    private final MenuModel menuModel;
+    private final GUIView view;
+    private final FrameRateController  frameRateController  = new FrameRateController (60);
     public MenuController(MenuModel menuModel, GUIView view){
         this.menuModel = menuModel;
         this.view = view;
     }
     
     public int run() throws IOException {
-        rateController.start();
-
         while(true){
-            rateController.startFrame();
+            frameRateController.startFrame();
             
-            long numberUpdates = rateController.numberUpdatesInThisFrame();
-            for(long i = 0; i < numberUpdates; ++i) {
+            while(true){
                 GUIView.COMMAND cmd = view.pollCommand();
-                if (!(cmd == null)) {
-                    switch (cmd) {
-                        case ESC:
-                            return -1;
-                        case UP:
-                            menuModel.selectAbove();
-                            break;
-                        case DOWN:
-                            menuModel.selectBelow();
-                            break;
-                        case ENTER:
-                            return menuModel.getSelectedItem().getId();
-                    }
+                if(cmd == null) break;
+                switch (cmd) {
+                    case ESC:
+                        return -1;
+                    case UP:
+                        menuModel.selectAbove();
+                        break;
+                    case DOWN:
+                        menuModel.selectBelow();
+                        break;
+                    case ENTER:
+                        return menuModel.getSelectedItem().getId();
                 }
             }
+            
             view.clear();
             view.draw();
             view.refresh();
--- src/main/java/com/pacman/g60/View/FrameRateController.java
+++ src/main/java/com/pacman/g60/View/FrameRateController.java
@@ -1,23 +1,39 @@
 package com.pacman.g60.View;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class FrameRateController {
-    private static final Integer SECONDS_TO_MILLIS = 1000;
+    private static final long SECONDS_TO_MILLIS = 1000L;
+    private static final long SECONDS_TO_NANOS = 1000000000L;
+    private static final long MILLIS_TO_NANOS = 1000000L;
     private Integer framesPerSecond;
     public FrameRateController(){ this(60); }
     public FrameRateController(Integer framesPerSecond){ setFramesPerSecond(framesPerSecond); }
-    public void setFramesPerSecond(Integer framesPerSecond){ this.framesPerSecond = framesPerSecond; }
-    private Integer getMillisPerFrame(){
-        return SECONDS_TO_MILLIS/ framesPerSecond;
+    public void setFramesPerSecond(Integer framesPerSecond){ 
+        this.framesPerSecond = framesPerSecond;
+        //nanosleep = getNanosPerFrame();
     }
-    private long previous = 0;
-    private long sleep = 0;
-    public void start(){ previous = System.currentTimeMillis(); }
-    public void startFrame() throws InterruptedException {
-        long current = System.currentTimeMillis();
-        long elapsed = current-previous;
-        previous = current;
-        if(elapsed < getMillisPerFrame()) ++sleep;
-        else if(elapsed > getMillisPerFrame()) sleep = Math.max(0, sleep-1);
-        Thread.sleep(sleep);
+    private long getNanosPerFrame(){
+        return SECONDS_TO_NANOS/ framesPerSecond;
+    }
+    private Queue<Long> times = new LinkedList<>();
+    private final static long Dt = SECONDS_TO_MILLIS; //one second
+    private long nanosleep = 0;
+    private long correctionFactor = 200;
+    public void startFrame() {
+        long now = System.currentTimeMillis();
+        times.add(now);
+        while(times.peek() != null && times.peek() < now-Dt) times.poll();
+        long nanoelapsed = Dt*MILLIS_TO_NANOS / times.size();
+        long nanosleepcorrection = getNanosPerFrame() - nanoelapsed;
+        nanosleep += nanosleepcorrection/correctionFactor;
+        nanosleep = Math.max(nanosleep, 0);
+        try {
+            Thread.sleep(nanosleep/MILLIS_TO_NANOS, (int) (nanosleep % MILLIS_TO_NANOS));
+        } catch (InterruptedException e) {
+            System.err.println("Interrupted: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
