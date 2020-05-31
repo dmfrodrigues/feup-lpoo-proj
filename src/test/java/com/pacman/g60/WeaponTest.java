6989be5ed208d8a20d841352bfb34578c7c8f2a5
MechJM <matosjoaops@protonmail.com>
2020-05-31 16:49:44 +0100

    Added tests for Weapon and subclasses

--- src/test/java/com/pacman/g60/WeaponTest.java
+++ src/test/java/com/pacman/g60/WeaponTest.java
@@ -1,4 +1,54 @@
 package com.pacman.g60;
 
+import com.pacman.g60.Model.Effect.DamageEffect;
+import com.pacman.g60.Model.Elements.Hero;
+import com.pacman.g60.Model.Elements.Knife;
+import com.pacman.g60.Model.Elements.Sword;
+import com.pacman.g60.Model.Elements.Weapon;
+import com.pacman.g60.Model.Models.ArenaModel;
+import com.pacman.g60.Model.Position;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class WeaponTest {
+    private Hero hero;
+    private ArenaModel arenaModel;
+
+    @Before
+    public void setup()
+    {
+        arenaModel = Mockito.mock(ArenaModel.class);
+        hero = Mockito.mock(Hero.class);
+        Mockito.when(arenaModel.getHero()).thenReturn(hero);
+        Mockito.doCallRealMethod().when(hero).setWeapon(Mockito.any(Weapon.class));
+        Mockito.when(hero.getWeapon()).thenCallRealMethod();
+    }
+
+    @Test
+    public void knife()
+    {
+        Weapon weapon = new Knife();
+        assertEquals(new DamageEffect(1),weapon.getEffect());
+
+        weapon.beCollected(arenaModel);
+        assertTrue(weapon.beCollected(arenaModel));
+        assertEquals(weapon,hero.getWeapon());
+    }
+
+    @Test
+    public void sword()
+    {
+        Weapon weapon = new Sword(new Position(1,1));
+        assertEquals(new DamageEffect(2),weapon.getEffect());
+
+        weapon.beCollected(arenaModel);
+        assertTrue(weapon.beCollected(arenaModel));
+        assertEquals(weapon,hero.getWeapon());
+    }
+
 }
